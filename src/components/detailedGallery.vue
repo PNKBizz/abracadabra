3871dcafe37e5cdf857b3323432a6b90f9417f37
fe-rsu-pavelgalkin <bizz.gc@gmail.com>
2017-04-17 23:42:10 +0300

    work with gallery

--- src/components/detailedGallery.vue
+++ src/components/detailedGallery.vue
@@ -1,29 +1,55 @@
 <template>
     <section class="detailedGallery">
+        <div class="detailedGallery__button detailedGallery__button--prev"
+             @click="prev" v-if="currentIndex < masterGallery.length"></div>
+        <div class="detailedGallery__button detailedGallery__button--next" @click="next"></div>
         <img :src="'/src/assets/gallery/' + master + '/' + imageId" alt="" class="detailedGallery__big-img">
         <aside class="detailedGallery__all-items">
-            <router-link tag="img"
-                         :to="'/works/' + master + '/' + item.name"
-                         :src="item.src"
-                         :alt="item.name"
-                         v-for="item in masterGallery"
-                         :key="item.name"
-                         class="detailedGallery__item"></router-link>
+            <div class="detailedGallery__all-items--container">
+                <router-link tag="img"
+                             :to="'/works/' + master + '/' + item.name"
+                             :src="item.src"
+                             :alt="item.name"
+                             v-for="item in masterGallery"
+                             :key="item.name"
+                             class="detailedGallery__item"></router-link>
+            </div>
         </aside>
     </section>
 </template>
 
 <script>
-	export default {
-		name: 'detailedGallery',
-		props: ['master', 'imageId'],
-        computed: {
-			masterGallery() {
-				if (!this.master) return [];
-				return this.$store.getters.getGalleryItems(this.master)
+    export default {
+        name: 'detailedGallery',
+        data() {
+            return {
+                currentIndex: null
             }
+        },
+        props: ['master', 'imageId'],
+        computed: {
+            masterGallery() {
+                if (!this.master) return [];
+                return this.$store.getters.getGalleryItems(this.master)
+            }
+        },
+        methods: {
+            next() {
+                this.$router.push('/works/' + this.master + '/' + this.masterGallery[++this.currentIndex].name)
+            },
+            prev() {
+                this.$router.push('/works/' + this.master + '/' + this.masterGallery[--this.currentIndex].name)
+            }
+        },
+        updated() {
+            const activeEl = document.querySelector('.detailedGallery__all-items .router-link-active');
+            activeEl.offsetParent.style.marginLeft = -activeEl.offsetLeft
+                + (+getComputedStyle(document.querySelector('.detailedGallery__all-items')).width.split('px')[0]
+                - +getComputedStyle(activeEl).width.split('px')[0]) / 2 + 'px';
+
+            this.currentIndex = this.masterGallery.findIndex(img => img.name === this.imageId);
         }
-	}
+    }
 </script>
 
 <style lang="scss">
@@ -33,6 +59,34 @@
         width: 100%;
         max-height: 100%;
         align-items: center;
+        position: relative;
+
+        &__button {
+            width: 70px;
+            height: 10px;
+            background-color: #2e2e2e;
+            position: absolute;
+            top: 35%;
+
+            &:after {
+                content: '';
+                height: 30px;
+                width: 10px;
+                background-color: inherit;
+                position: absolute;
+                left: 10px;
+                top: -100%;
+            }
+
+            &--prev {
+                left: 30px;
+            }
+
+            &--next {
+                right: 30px;
+                transform: scale(-1);
+            }
+        }
 
         &__big-img {
             height: 100%;
@@ -65,6 +119,14 @@
             overflow: hidden;
             border-top: 1px solid darkred;
             box-sizing: border-box;
+            position: relative;
+
+            &--container {
+                display: flex;
+                box-sizing: border-box;
+                position: relative;
+                height: 100%;
+            }
         }
     }
 </style>
